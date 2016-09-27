b5f3acc685327967eea869b0980bf9c233407b4c
Thiti Watanasrimongkol <golf.thiti@gmail.com>
2016-09-27 07:38:41 +0700

    Add data seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,7 +11,24 @@
 # and so on) as they will fail if something goes wrong.
 alias Sas.Repo
 alias Sas.Category
+alias Sas.Table
+alias Sas.User
 
 for category <- ~w(Alcohol Cocktail Mixer Snack) do
   Repo.insert!(%Category{name: category})
 end
+
+name = "bar"
+pass = "bar"
+
+Repo.insert!(%Table{name: name, password: pass})
+
+name = "admin"
+pass = "admin"
+role = User.admin
+
+pass_hash = Comeonin.Bcrypt.hashpwsalt(pass)
+
+admin = Repo.get_by!(User, name: name)
+Repo.delete(admin)
+Repo.insert!(%User{name: name, password_hash: pass_hash, role: role})
